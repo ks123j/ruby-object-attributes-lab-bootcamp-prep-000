f16dfcecbf804cb1d7032b778cd0d0c9407a3263
Katy <komal.vadukul@gmail.com>
2018-06-06 20:43:48 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=
+    @name = name
+  end
+
+  def name
+    @name = name
+  end
+
+  def breed=
+    @breed = breed
+  end
+
+  def breed
+    @breed = breed
+  end
+
+end
