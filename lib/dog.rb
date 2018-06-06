911bee38614d20b178062add2d5b000e8ba34ddf
Katy <komal.vadukul@gmail.com>
2018-06-06 20:53:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,3 @@
 class Dog
-  def names
-    @names
-  end
-
-  def names=
-    @names = new_name
-  end
-
-  def breeds=
-    @breeds = new_breed
-  end
-
-  def breeds
-    @breeds
-  end
-
+  
 end
