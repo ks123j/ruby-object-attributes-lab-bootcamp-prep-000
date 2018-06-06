b1516f1f6e256368a2dfc7714fdd2f41cf1435c8
Katy <komal.vadukul@gmail.com>
2018-06-06 20:53:17 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,3 @@
 class Person
-  def names=(new_name)
-    @name = new_name
-  end
-
-  def names
-    @names
-  end
-
-  def jobs=
-    @jobs = new_job
-  end
-
-  def jobs
-    @jobs
-  end
-
+  
 end
